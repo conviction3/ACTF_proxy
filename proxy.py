67ef7f0efd539e7ee037bd92f80f7b1202853846
Bian <374044291@qq.com>
2021-04-05 19:27:59 +0800

    + clean code

--- proxy.py
+++ proxy.py
@@ -34,14 +34,12 @@ class Proxy:
     seq from 0 to 8, then the job is done, the ordered packages will be combined into a one single huger
     package then send to the sever.
     """
-    TARGET_SEQ_DATA_NUM = 3
+    TARGET_SEQ_DATA_NUM = 10
     MAX_BUFFER = 10
 
     def __init__(self, socket: Socket):
         self.socket: Socket = socket
         self.client_list: List[Client] = []
-
-        # self.assigned_data: List[RowDataDesc] = []
 
         self.consuming_count: int = 0
 
@@ -53,14 +51,12 @@ class Proxy:
         there will be another thread to handle the buffer to reorder the package by package seq.
         If the this buffer is full, then should limit the sending speed of clients.
             Actually, the received_buffer and the ordered_packages is a producer-consumer model.
-        The received buffer is a critical resources, the thread which receives packages from 
-        clients is producers, and the thread which retrieves data from the buffer is a consumer.
+        The received buffer is a shared resources, the thread which receives packages from clients
+        is producers, and the thread which retrieves data from the buffer is a consumer.
             The item in queue is class SeqData
         """
         self.received_buffer: queue.Queue = queue.Queue(maxsize=Proxy.MAX_BUFFER)
         """
-        #     List of packages. When the proxy had received packages from client, it will be placed the
-        # package into the specific position of the list by the package seq.
             List of ordered packages, the size of this list is grater than received_buffer a lot.
         This list could be stored in files or database orderly, then combine to one package or do some
         calculation before sending to server.
@@ -86,7 +82,6 @@ class Proxy:
 
     def start_consume(self):
         """
-            Consume the
         :return:
         """
 
@@ -156,7 +151,6 @@ class Proxy:
                                      sock=client.socket)
                         self.print_buffer()
                     # <--- parse and handle the package
-
                 except (ConnectionAbortedError, ConnectionResetError):
                     break
 
@@ -197,81 +191,82 @@ class Proxy:
         package.get_header().set_message("Total count")
         send_package(package, s)
 
-    # def check_ordered_packages(self, _log: False) -> bool:
-    #     """
-    #         Check the status of ordered package list
-    #     :param _log: print the status or not
-    #     :return: False: if the list is not full
-    #     """
-    #     none_count = 0
-    #     for item in self.ordered_packages:
-    #         if item is None and not _log:
-    #             return False
-    #         if item is None and _log:
-    #             none_count += 1
-    #     if _log:
-    #         log.debug(f"{len(self.ordered_packages) - none_count} of {len(self.ordered_packages)}"
-    #                   f" ordered packages is filled")
-    #     if none_count == 0:
-    #         return True
-    #     else:
-    #         return False
+# ---------------------------> deprecated for now <---------------------------------------
+# def check_ordered_packages(self, _log: False) -> bool:
+#     """
+#         Check the status of ordered package list
+#     :param _log: print the status or not
+#     :return: False: if the list is not full
+#     """
+#     none_count = 0
+#     for item in self.ordered_packages:
+#         if item is None and not _log:
+#             return False
+#         if item is None and _log:
+#             none_count += 1
+#     if _log:
+#         log.debug(f"{len(self.ordered_packages) - none_count} of {len(self.ordered_packages)}"
+#                   f" ordered packages is filled")
+#     if none_count == 0:
+#         return True
+#     else:
+#         return False
 
-    # def start_receive_thread(self, client: Client):
-    #     def temp():
-    #         while True:
-    #             result = receive_package(client.socket)
-    #             if isinstance(result, Header):
-    #                 header = result
-    #                 log.debug(f"<- message: \"{header.get_message()}\" "
-    #                           f"| hash: {header.get_package_hashcode()}")
-    #             else:
-    #                 package = result
-    #                 header = result.get_header()
-    #                 log.debug("<- " + package.get_desc())
-    #                 if header.has_ack():
-    #                     log.info(
-    #                         f"Slice {header.get_ack(parse=True)} done by client {client.uuid}, "
-    #                         f"result = {package.get_payload(parse=True)}")
-    #
-    #     t = Thread(target=temp)
-    #     t.start()
+# def start_receive_thread(self, client: Client):
+#     def temp():
+#         while True:
+#             result = receive_package(client.socket)
+#             if isinstance(result, Header):
+#                 header = result
+#                 log.debug(f"<- message: \"{header.get_message()}\" "
+#                           f"| hash: {header.get_package_hashcode()}")
+#             else:
+#                 package = result
+#                 header = result.get_header()
+#                 log.debug("<- " + package.get_desc())
+#                 if header.has_ack():
+#                     log.info(
+#                         f"Slice {header.get_ack(parse=True)} done by client {client.uuid}, "
+#                         f"result = {package.get_payload(parse=True)}")
+#
+#     t = Thread(target=temp)
+#     t.start()
 
-    # def __get_data_slice(self) -> Tuple[int, int]:
-    #     """
-    #         Supposed the csv file should be split into different data to send to
-    #     different clients. There must be a method to deal with the slice of the
-    #     data.
-    #         The slice should be determined by the number of clients、the data has
-    #     been assigned and has been finished by client, and the data that has been
-    #     assigned but not been finished ( client offline or timeout ).
-    #     :return:
-    #     """
-    #     if len(self.assigned_data) == 0:
-    #         return 0, 10
-    #
-    #     _max_slice_left = 0
-    #     for row_data in self.assigned_data:
-    #         # todo: reassigned the failed data
-    #         # find the max slice
-    #         _max_slice_left = max(row_data.slice[0], _max_slice_left)
-    #     return _max_slice_left, _max_slice_left + 10
-    #
-    # def send_raw_data(self, client: Client):
-    #     file_name = "./data/distribution_add_data_1.csv"
-    #     # load data from file
-    #     _slice = self.__get_data_slice()
-    #     slice_data = read_csv_int(file_name, _slice[0], _slice[1])
-    #     row_data_desc = RowDataDesc(_slice=_slice, _hash=get_obj_hash(slice_data), file_name=file_name)
-    #
-    #     package = Package(payload=int_list_to_bytes(slice_data), data_type=PackageDataType.INT)
-    #     msg = "transmit raw data"
-    #     package.generate_default_header(msg)
-    #     send_package(package, client.socket)
-    #
-    #     log.debug(
-    #         f"-> Sending raw data[{_slice[0]}:{_slice[1]}] to client \"{client.uuid}\" | "
-    #         + package.get_desc())
+# def __get_data_slice(self) -> Tuple[int, int]:
+#     """
+#         Supposed the csv file should be split into different data to send to
+#     different clients. There must be a method to deal with the slice of the
+#     data.
+#         The slice should be determined by the number of clients、the data has
+#     been assigned and has been finished by client, and the data that has been
+#     assigned but not been finished ( client offline or timeout ).
+#     :return:
+#     """
+#     if len(self.assigned_data) == 0:
+#         return 0, 10
+#
+#     _max_slice_left = 0
+#     for row_data in self.assigned_data:
+#         # todo: reassigned the failed data
+#         # find the max slice
+#         _max_slice_left = max(row_data.slice[0], _max_slice_left)
+#     return _max_slice_left, _max_slice_left + 10
+#
+# def send_raw_data(self, client: Client):
+#     file_name = "./data/distribution_add_data_1.csv"
+#     # load data from file
+#     _slice = self.__get_data_slice()
+#     slice_data = read_csv_int(file_name, _slice[0], _slice[1])
+#     row_data_desc = RowDataDesc(_slice=_slice, _hash=get_obj_hash(slice_data), file_name=file_name)
+#
+#     package = Package(payload=int_list_to_bytes(slice_data), data_type=PackageDataType.INT)
+#     msg = "transmit raw data"
+#     package.generate_default_header(msg)
+#     send_package(package, client.socket)
+#
+#     log.debug(
+#         f"-> Sending raw data[{_slice[0]}:{_slice[1]}] to client \"{client.uuid}\" | "
+#         + package.get_desc())
 
 # class RowDataDesc:
 #     def __init__(self, _slice: Tuple[int, int], _hash: str, file_name: str = None):
@@ -281,3 +276,4 @@ class Proxy:
 #         self.start_time = time.time()
 #         self.end_time = None
 #         self.finished = False
+# ---------------------------> deprecated for now <---------------------------------------
